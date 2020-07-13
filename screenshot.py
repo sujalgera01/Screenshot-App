7d3619f4b873b1bea39eca2dc74067b37d188e6b
sujalgera01 <sujalgera01@gmail.com>
2020-07-13 11:46:45 +0530

    initial commits

---
+++ screenshot.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui
+
+def screenshot():
+    name = int(round(time.time()*1000))
+    name = "C:/Users/sujal/Desktop/codes/Screenshot-App/images/{}.png".format(name)
+    time.sleep(7)
+    img = pyautogui.screenshot(name)
+    img.show()
+
+screenshot()
